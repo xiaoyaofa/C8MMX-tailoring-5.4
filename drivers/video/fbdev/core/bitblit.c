7ee7bc4dc5894c1e27116bc5a215961200fd4efc
xiaoyaofa <1240980018@qq.com>
2024-11-06 15:02:04 +0800

    close lcd cursor

--- drivers/video/fbdev/core/bitblit.c
+++ drivers/video/fbdev/core/bitblit.c
@@ -379,11 +379,11 @@ static void bit_cursor(struct vc_data *vc, struct fb_info *info, int mode,
 	cursor.image.depth = 1;
 	cursor.rop = ROP_XOR;
 
-	if (info->fbops->fb_cursor)
-		err = info->fbops->fb_cursor(info, &cursor);
+	//if (info->fbops->fb_cursor)
+	//	err = info->fbops->fb_cursor(info, &cursor);
 
-	if (err)
-		soft_cursor(info, &cursor);
+	//if (err)
+	//	soft_cursor(info, &cursor);
 
 	ops->cursor_reset = 0;
 }
